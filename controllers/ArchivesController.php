794c7a23e972ea563cbd04e715128f96b9d3e9d9
Gautier Michelin <gm@ideesculture.com>
2018-11-13 15:49:30 +0100

    Sorting by idno and code cleaning

--- controllers/ArchivesController.php
+++ controllers/ArchivesController.php
@@ -27,7 +27,7 @@
         protected $opa_list_of_lists; // list of lists
         protected $opa_listIdsFromIdno; // list of lists
         protected $opa_locale; // locale id
-
+		private $opo_list;
  		# -------------------------------------------------------
  		# Constructor
  		# -------------------------------------------------------
@@ -42,7 +42,7 @@
  			}*/
 
  			$this->opo_config = Configuration::load(__CA_APP_DIR__.'/plugins/archives/conf/archives.conf');
-
+			$this->opo_list = new ca_lists("object_types");
         }
 
  		# -------------------------------------------------------
@@ -51,7 +51,7 @@
  		public function Index($type="") {
  			$id= $this->request->getParameter("id", pInteger);
 			$type_id=$this->opo_config->get("root_type");
-			$query = "SELECT ca_object_labels.object_id, idno, name FROM ca_objects left join ca_object_labels ON ca_objects.object_id = ca_object_labels.object_id AND is_preferred=1";
+			$query = "SELECT ca_object_labels.object_id, idno, name FROM ca_objects left join ca_object_labels ON ca_objects.object_id = ca_object_labels.object_id AND is_preferred=1 AND deleted=0";
 			if(!$id) {
 				$this->view->setVar("object_id", false);
 				$query .= " WHERE ca_objects.type_id =".$type_id." and deleted=0";
@@ -61,6 +61,7 @@
 				$this->view->setVar("object_id", $id);
 				$query .= " WHERE parent_id =".$id." and deleted=0";
 			}
+			$query .= " ORDER BY idno ASC";
 			$o_data = new Db();
 			$this->view->setVar("qr_result", $o_data->query($query));
  			$this->view->setVar("template", $this->opo_config->get("template"));
@@ -70,18 +71,19 @@
  		public function Fetch($type="") {
 			$id = $this->request->getParameter("id", pInteger);
 			$level = $this->request->getParameter("level", pInteger);
-			$query = "SELECT ca_object_labels.object_id, idno, name FROM ca_objects left join ca_object_labels ON ca_objects.object_id = ca_object_labels.object_id AND is_preferred=1";
+			$query = "SELECT ca_object_labels.object_id, idno, name FROM ca_objects left join ca_object_labels ON ca_objects.object_id = ca_object_labels.object_id AND is_preferred=1 AND deleted=0";
 			if(!$id) {
 				$query .= " WHERE ca_objects.type_id =".$this->opo_config->get('root_type')." and deleted=0";
 			} else {
 				$query .= " WHERE ca_objects.parent_id =".$id." and deleted=0";
 			}
+			$query .= " ORDER BY idno ASC";
 			$o_data = new Db();
 			$qr_result=$o_data->query($query);
 
 			$result_rows = [];
 			while($qr_result->nextRow()) {
-				$query2 = "SELECT count(object_id) as numchildren FROM ca_objects WHERE parent_id = ".$qr_result->get('object_id');
+				$query2 = "SELECT count(object_id) as numchildren FROM ca_objects WHERE parent_id = ".$qr_result->get('object_id')." AND deleted=0";
 				$qr2_result = $o_data->query($query2);
 				$qr2_result->nextRow();
 				$numchildren=$qr2_result->getRow()["numchildren"];
@@ -94,8 +96,10 @@
 			$this->view->setVar("result_rows", $result_rows);
 			$this->view->setVar("level", $level);
 
-			$this->view->setVar("template", $this->opo_config->get("template"));
+			$this->view->setVar("template", $this->ConvertValuesToIdsInsideTemplate($this->opo_config->get("displayTemplate")));
             $this->view->setVar("printLevel", $this->opo_config->get("printLevel"));
+            $this->view->setVar("pdfLevel", $this->opo_config->get("pdfLevel"));
+            $this->view->setVar("exportLevel", $this->opo_config->get("exportLevel"));
 			print $this->render('fetch_html.php');
 			exit();
 		}
@@ -106,17 +110,68 @@
 			$this->view->setVar("object_id", $id);
 			$item = new ca_objects($id);
 			$this->view->setVar("item", $item);
-			$this->view->setVar("template", $this->opo_config->get("template"));
-			$this->view->setVar("inner_template", $this->opo_config->get("inner_template"));
-			$this->view->setVar("templateH1", $this->opo_config->get("templateH1"));
+			$this->view->setVar("template", $this->ConvertValuesToIdsInsideTemplate($this->opo_config->get("printTemplate")));
 			$result = $this->render('export_html.php');
-			print $result;die();
-
-			$renderer = new WLPlugPDFRendererPhantomJS();
-			$renderer->setPage("A4", "portrait", "2.5cm", "1cm", "2.5cm", "1cm");
-			$renderer->render($result, ["stream"=>true, "filename"=>"fonds.pdf" ]);
+			print $result;
 			exit();
 		}
 
+        public function Pdf() {
+            //error_reporting(E_ALL);
+            $id= $this->request->getParameter("id", pInteger);
+            $this->view->setVar("object_id", $id);
+
+            $item = new ca_objects($id);
+            $this->view->setVar("item", $item);
+            $this->view->setVar("template", $this->ConvertValuesToIdsInsideTemplate($this->opo_config->get("printTemplate")));
+            $result = $this->render('pdf_html.php');
+
+            $export_file = "archives_export_pdf_".$id.".html";
+            $path = __DIR__."/../temp/".$export_file;
+            unlink($path);
+            file_put_contents($path, $result);
+            $command = "cd ".__DIR__."/../temp && phantomjs rasterize.js archives_export_pdf_".$id.".html archives_export_pdf_".$id.".pdf A4";
+            //print $command;
+            exec($command, $output);
+
+            $files = [];
+            //var_dump(__DIR__."/../temp/".$id);die();
+            foreach(scandir(__DIR__."/../temp/".$id) as $file) {
+                if(strpos($file,"_")>0 && (substr($file, -4)==".pdf")) {
+                    $num = reset(explode("_", $file));
+                    if(!$num*1) {
+                        continue;
+                    }
+                    $files[] = $id."/".$file;
+                }
+
+            }
+            $file = __DIR__."/../temp/".$id.".pdf";
+            unlink($file);
+            
+            $command = "sleep 3 && cd ".__DIR__."/../temp && pdftk ".implode(" ", $files)." archives_export_pdf_".$id.".pdf cat output ".$id.".pdf";
+            exec($command, $output);
+
+			print json_encode(
+				[
+					"message"=>"Le fichier PDF est en cours de création",
+					"URL"=>"/gestion/app/plugins/archives/temp/".$id.".pdf"
+				]	
+			);
+
+			die();
+        }
+
+        private function ConvertValuesToIdsInsideTemplate($template) {
+            foreach($template as $key=>$template_per_id) {
+            	if($key == "_default") continue;
+                //print $key."\n";
+                $type_id  = $this->opo_list->getItemIDFromListByItemValue("object_types", $key);
+                $template[$type_id] = $template[$key];
+                unset($template[$key]);
+            }
+            return $template;
+
+        }
  	}
  ?>
