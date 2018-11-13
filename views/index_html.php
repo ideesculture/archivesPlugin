14ad89f1d3ee4e10daa1d4ed6d1afe5680cd4fee
Gautier Michelin <gm@ideesculture.com>
2018-11-13 15:57:58 +0100

    css

--- views/index_html.php
+++ views/index_html.php
@@ -24,7 +24,7 @@
 		margin-bottom:5px;
 		display:inline-block;
 	}
-	a[id^="expandButton"], a[id^="shrinkButton"] {
+	a[id^="expandButton"], a[id^="shrinkButton"], a[id^="exportButton"], a[id^="printButton"] {
 		color:black;
 		cursor:pointer;
 		text-decoration: none;
@@ -32,7 +32,7 @@
 </style>
 <script>
 	jQuery("document").ready(function() {
-		var url = "/gestion/index.php/archives/archives/Fetch";
+		var url = "<?php print __CA_URL_ROOT__; ?>/index.php/archives/archives/Fetch";
 		console.log(url);
 		jQuery.get(url, function(data){
 			jQuery('#hierarchyRoot').html(data);
@@ -55,5 +55,19 @@
 		jQuery("#shrinkButton"+object_id).parent().prepend("<a onClick='expandHierarchy("+object_id+","+level+");' id='expandButton"+object_id+"'><i class='fa fa-plus-square-o'></i></a>");
 		jQuery("#shrinkButton"+object_id).remove();
 		jQuery("#hierarchyFor"+object_id).html("");
+	}
+	
+	var generateArchivesPdf = function(object_id) {
+		jQuery("#pdfButton"+object_id+" i").removeClass("fa-file-pdf-o").addClass("fa-spinner fa-pulse fa-fw");
+		url='/gestion/index.php/archives/archives/Pdf/id/'+object_id;
+		var that=object_id;
+		jQuery.getJSON(url, function(data) {
+			console.log(data);
+			jQuery("#pdfButton"+that).prop('onclick',null).off('click');
+			jQuery("#pdfButton"+that+" i").removeClass("fa-spinner fa-pulse fa-fw").addClass("fa-file-pdf-o");
+			jQuery("#pdfButton"+that+" a").attr("href", data.URL);
+			jQuery("#pdfButton"+that+" a").css("color", "#1ab3c8a");
+			
+		})
 	}
 </script>
