2e6642ac2033acb41fc88b7f6a0503084218084e
Gautier Michelin <gm@ideesculture.com>
2018-11-13 15:58:36 +0100

    code simplification

--- views/export_html.php
+++ views/export_html.php
@@ -1,46 +1,43 @@
 <header>
 	<meta charset="UTF-8">
 </header>
-<body onload='window.print()'>
+<body>
+<!-- <body onload='window.print()'> -->
 <?php
 error_reporting(E_ERROR);
 	$id = $this->getVar("object_id");
 	$item = $this->getVar("item");
 	$template = $this->getVar("template");
-	$inner_template = $this->getVar("inner_template");
-	$templateH1 = $this->getVar("templateH1");
 	$db = new Db();
 	error_reporting(E_ERROR);
 
 	function printItemWithTemplate ($item, $template) {
-		print $item->getWithTemplate($template);
+	    $type = $item->get("ca_objects.type_id");
+	    if(isset($template[$type]) && trim($template[$type])) {
+		    $return = $item->getWithTemplate($template[$type]);
+        } else {
+            $return = $item->getWithTemplate($template["_default"]);
+        }
+        print $return;
 	}
 
 
-	function printHierarchyFromItemWithTemplate($db, $id, $template, $inner_template, $level) {
+	function printHierarchyFromItemWithTemplate($db, $id, $template, $level) {
 		$item = new ca_objects($id);
 		print "<blockquote>";
-		if($level==1) {
-			printItemWithTemplate($item, "<h1>".$template."</h1>");
-		} elseif($level<5) {
-			//printItemWithTemplate($item, "<h$level>".$template."</h$level>");
-			printItemWithTemplate($item, "<h3>".$template."</h3>");
-		} else {
-			printItemWithTemplate($item, "<p>".$template."</p>");
-		}
-		printItemWithTemplate($item, $inner_template);
+        printItemWithTemplate($item, $template);
 		$num_children = reset($item->getHierarchyChildCountsForIDs([$id]));
 		if($num_children) {
 			$children = $item->getHierarchyChildren($id);
 			foreach($children as $child) {
-				printHierarchyFromItemWithTemplate($db, $child["object_id"], $template, $inner_template, $level+1);
+				printHierarchyFromItemWithTemplate($db, $child["object_id"], $template, $level+1);
 			}
 		}
 		print "</blockquote>";
 	}
 
 	//printItemWithTemplate($item, $templateH1);
-	printHierarchyFromItemWithTemplate($db, $id, $template, $inner_template, 1);
+	printHierarchyFromItemWithTemplate($db, $id, $template, 1);
 ?>
 <style>
 body {
@@ -55,7 +52,6 @@ h1, h2, h3, h4, h5, h6 {
 	blockquote blockquote {
 		border-left:1px solid lightgray;
 		padding-left:10px;
-		border-top:1px solid lightgray;
 	}
 </style>
 </body>
